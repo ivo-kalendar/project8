82b88fe48ab148fff4ed309716f21cf925075046
Ivo <ivokalendar@icloud.com>
2020-03-24 18:40:11 +0100

    Seample props exercises...

--- src/frontend/react/nav/nav.jsx
+++ src/frontend/react/nav/nav.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
-import Test from '../test.jsx'
+import Child from '../child.jsx'
+import Parent from '../parent.jsx'
 
 const Nav = () => {
-	return(
+	return (
 		<nav>
-			<h1>{Test.statement ? Test.seampleTest : Test.anoteherTest}</h1>
+			<Parent>
+				<h3><Child name="Sam" time="09:33" /></h3>
+				<h1><Child name="Din" time="04:55" state /></h1>
+			</Parent>
+			<Parent>
+				<h3><Child name="Victor" time="13:25" /></h3>
+				<h1><Child name="Sasha" time="10:25" /></h1>
+				<h1><Child name="Jack" time="11:25" /></h1>
+			</Parent>
+			<Parent>
+				<h3><Child name="Linda" time="10:05" /></h3>
+				<h3><Child name="John" time="03:25" state={true} /></h3>
+			</Parent>
 		</nav>
 	)
 }
--- src/frontend/react/test.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Test = {
-	seampleTest: <span className="green alert">This is showing beceouse of true statement...</span>,
-	anoteherTest: <span className="red alert">This is false statement...</span>,
-	statement: false
-}
-
-export default Test
